52658f3bbd1a713a4b794951fc6607bc4055061a
vismaya-dinesh <vismayadineshkumar@gmail.com>
2026-03-08 22:21:44 +0530

    bug fixed

--- backend/app.py
+++ backend/app.py
@@ -8,6 +8,9 @@ CORS(app)
 
 database_url = os.getenv("DATABASE_URL")
 
+if database_url and database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+
 if database_url:
     app.config["SQLALCHEMY_DATABASE_URI"] = database_url
 else:
